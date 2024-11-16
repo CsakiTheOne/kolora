53c56826fefe65dba7406027c27c732671aca5f3
Csáki <jockahun@gmail.com>
2024-11-16 11:29:04 +0100

    Path done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,65 @@
 <script>
+    import { onMount } from "svelte";
     import Footer from "../components/Footer.svelte";
-
 </script>
-<main>
-    <p>Művészet?</p>
-    <p>Kultúra?</p>
-    <p>Székesfehérvár?</p>
-    <h1 id="kolora-title">kolora!</h1>
-    <p>
-        <a href="/timeline">Idővonal</a>
-    </p>
-    <p>
-        <a href="/memories-24-25">2024-25</a>
-    </p>
-    <p>
-        <a href="/contacts">Elérhetőségek</a>
-    </p>
-</main>
+
+<div class="container">
+    <svg viewBox="0 0 100 100" preserveAspectRatio="none">
+        <path fill="#fff" d="M 0 0 V 100 H 100 V 10 C 93 6 81 3 70 3 C 59 3 48 11 37 11 C 25 11 16 0 0 0 Z" />
+    </svg>
+    <main>
+        <p>Művészet?</p>
+        <p>Kultúra?</p>
+        <p>Székesfehérvár?</p>
+        <h1 id="kolora-title">kolora!</h1>
+        <p>
+            <a href="/timeline">Idővonal</a>
+        </p>
+        <p>
+            <a href="/memories-24-25">2024-25</a>
+        </p>
+        <p>
+            <a href="/contacts">Elérhetőségek</a>
+        </p>
+    </main>
+</div>
 <Footer />
 
 <style>
-    main {
+    .container {
         background-color: var(--primary-color);
+        position: relative;
+        width: 100%;
+        height: 100svh;
+    }
+
+    .container > * {
+        position: absolute;
+    }
+
+    main {
+        left: 0;
+        right: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
         color: var(--on-primary-color);
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: space-evenly;
-        height: 100svh;
     }
 
-    a {
-        color: var(--on-primary-color);
+    svg {
+        display: block;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 45%;
+    }
+
+    svg > path {
+        fill: var(--primary-light-color);
     }
 </style>
