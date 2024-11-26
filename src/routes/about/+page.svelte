2dd1b9577900357fdbbcbffac5699b965bb47fec
Csáki <jockahun@gmail.com>
2024-11-26 18:44:15 +0100

    Added place to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,12 +12,20 @@
     </p>
     <h2>Hogyan?</h2>
     <p>
-        Művészeti / kulturális rendezvényeinken és közösségi média
-        profiljainkon rendszeres bemutatkozási és fellépési lehetőséggel
-        támogatjuk a helyi feltörekvő művészeket.
+        Művészeti / kulturális rendezvényeinken és közösségi média profiljainkon
+        rendszeres bemutatkozási és fellépési lehetőséggel támogatjuk a helyi
+        feltörekvő művészeket.
     </p>
-    <p>
-        Legyen Székesfehérvár tele élettel!
-    </p>
+    <h2>Hol?</h2>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d173512.4889738877!2d18.27644473538632!3d47.19441633333276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4769f793764a78df%3A0x400c4290c1e11f0!2zU3rDqWtlc2ZlaMOpcnbDoXI!5e0!3m2!1sen!2shu!4v1732642994688!5m2!1sen!2shu"
+        width="100%"
+        height="350"
+        style="border:0;"
+        allowfullscreen
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+    <p>Legyen Székesfehérvár tele élettel!</p>
 </main>
 <Footer />
