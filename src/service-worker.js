7ac63f2211b192c6631879f6bf70aca39b35155d
Csáki <jockahun@gmail.com>
2024-12-21 00:13:42 +0100

    Service worker

---
+++ src/service-worker.js
@@ -0,0 +1,11 @@
+// This is the "Offline copy of pages" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
