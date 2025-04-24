0ad46d6e30926e4cce8f3a88730e04b88e904b37
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2025-04-24 20:36:05 +0200

    Refactor ScrollToTopButton to use canvas for rendering and improve scroll event handling

--- src/components/ScrollToTopButton.svelte
+++ src/components/ScrollToTopButton.svelte
@@ -1,55 +1,69 @@
 <script lang="ts">
     import { onMount } from "svelte";
 
-    let btn: HTMLButtonElement | null = null;
+    let scrollY = $state(0);
+    let btn: HTMLCanvasElement | null = $state(null);
+    let ctx: CanvasRenderingContext2D | null = $state(null);
 
     onMount(() => {
-        function scrollListener() {
-            if (!btn) return;
-            if (window.scrollY > 350) {
-                btn.style.display = "block";
-            } else {
-                btn.style.display = "none";
-            }
-            btn.style.opacity = "" + window.scrollY / 2000;
+        if (!btn) return;
+        ctx = btn.getContext("2d");
+    });
+
+    $effect(() => {
+        if (!btn || !ctx) return;
+
+        ctx.clearRect(0, 0, btn.width, btn.height);
+        ctx.beginPath();
+        const offset = -scrollY / 100;
+        for (let i = 0; i < 2 * Math.PI; i += 0.1) {
+            const radius = 25 + Math.sin(offset + i * Math.PI * 2.25) * 2;
+            ctx.lineTo(32 + radius * Math.cos(i), 32 + radius * Math.sin(i));
         }
+        ctx.closePath();
+        ctx.fillStyle =
+            btn.computedStyleMap().get("color")?.toString() || "#000";
+        ctx.fill();
+        ctx.fillStyle = "#fff";
+        ctx.fill(
+            new Path2D(
+                "M 21 23 H 43 V 25 H 21 Z M 31.5 25 L 36.5 30.5 H 32.5 V 45 H 30.5 V 30.5 H 26.5 Z",
+            ),
+        );
 
-        document.addEventListener("scroll", scrollListener);
-
-        return () => {
-            document.removeEventListener("scroll", scrollListener);
-        };
+        if (scrollY > 350) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
+        btn.style.opacity = "" + scrollY / 2000;
     });
 </script>
 
-<button
+<svelte:window bind:scrollY />
+<canvas
     bind:this={btn}
+    width="64"
+    height="64"
     onclick={() => {
         window.scrollTo({
             top: 0,
             behavior: "smooth",
         });
     }}
+    role="button"
     aria-label="Scroll to top"
 >
-    <span class="mdi mdi-format-vertical-align-top"></span>
-</button>
+</canvas>
 
 <style>
-    button {
-        display: none;
-        opacity: 0;
+    canvas {
         position: fixed;
-        bottom: calc(var(--spacing) * 2);
         right: calc(var(--spacing) * 2);
-        font-size: 1.3rem;
-        padding: calc(var(--spacing) / 2);
-        appearance: none;
-        background-color: var(--primary-color);
-        color: var(--on-primary-color);
-        border: none;
-        border-radius: var(--corner-radius);
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        bottom: calc(var(--spacing) * 2);
+        width: 64px;
+        height: 64px;
+        color: var(--primary-color);
         cursor: pointer;
     }
 </style>
