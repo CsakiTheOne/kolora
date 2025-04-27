a229176cbe2dae2d6f631d4b6f1387a030a1dbd1
Csáki <jockahun@gmail.com>
2025-04-27 21:54:41 +0200

    Experiment members page

---
+++ src/components/KoloraMemberDisplay.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import { enableMultiTabIndexedDbPersistence } from "firebase/firestore";
+    import Badge from "./Badge.svelte";
+
+    const {
+        profilePictureUrl = "",
+        name,
+        tags = [],
+        tagsColor = null,
+        bio = "",
+        links = [],
+    } = $props();
+</script>
+
+<div class="member">
+    <img
+        src={profilePictureUrl}
+        alt="Kolora tag képe"
+        style="background: var(--primary-variant-color);"
+    />
+    <div class="member-info">
+        <div>
+            <h3>{name}</h3>
+            <div
+                style="display: flex; flex-direction: row; gap: calc(var(--spacing) / 2); padding-top: calc(var(--spacing) / 4);"
+            >
+                {#each tags as tag}
+                    <Badge style="background: {tagsColor}; color: white;">
+                        {tag}
+                    </Badge>
+                {/each}
+            </div>
+        </div>
+        <p>
+            {#if bio.length > 0}
+                {bio}
+            {:else}
+                -
+            {/if}
+        </p>
+        {#if links.length > 0}
+            <ul>
+                {#each links as link}
+                    <a href={link.url} target="_blank">
+                        <li>{link.name}</li>
+                    </a>
+                {/each}
+            </ul>
+        {/if}
+    </div>
+</div>
+
+<style>
+    .member {
+        display: flex;
+        flex-direction: row;
+        gap: var(--spacing);
+    }
+
+    .member > img {
+        width: 64px;
+        height: 64px;
+        border-radius: 50%;
+    }
+
+    .member-info {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        gap: calc(var(--spacing) / 2);
+    }
+</style>
--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,10 +1,69 @@
 <script>
     import Footer from "../../components/Footer.svelte";
     import Header from "../../components/Header.svelte";
+    import KoloraMemberDisplay from "../../components/KoloraMemberDisplay.svelte";
 </script>
 
 <Header selectedTab="Tagok" />
 <main>
     <h2>Tagok</h2>
+
+    <!-- Elnökség -->
+    <KoloraMemberDisplay name="Völgyesi Tamás - Toma" tags={["Elnök"]} tagsColor="var(--primary-color)" />
+    <KoloraMemberDisplay name="Tóth Nikolett" tags={["Alelnök"]} tagsColor="var(--primary-color)" />
+    <KoloraMemberDisplay
+        name="Bodonyi Dániel"
+        tags={["Alelnök"]}
+        tagsColor="var(--primary-color)"
+        links={[
+            {
+                name: "Café at Museum Essentials mix",
+                url: "https://www.mixcloud.com/Daniel_Bodonyi/bodonyi-dani-caf%C3%A9-at-museum-essentials/",
+            },
+        ]}
+    />
+    <!-- Tagok -->
+    <KoloraMemberDisplay name="Gergely Emma" tags={["Táncos"]} />
+    <KoloraMemberDisplay
+        name="Mészáros Emese"
+        tags={["Fotós"]}
+        links={[
+            {
+                name: "@photos.m.e fotós Insta",
+                url: "https://www.instagram.com/photos.m.e/",
+            },
+        ]}
+    />
+    <KoloraMemberDisplay
+        name="Reszegi Ramóna"
+        tags={["Énekes"]}
+        links={[
+            {
+                name: "Nedel'a zenekar",
+                url: "https://linktr.ee/nedela_zenekar",
+            },
+        ]}
+    />
+    <KoloraMemberDisplay
+        name="Szvoren Eszter"
+        links={[
+            {
+                name: "Irodalmi mémek a @krudy_froccs Instagramon",
+                url: "https://www.instagram.com/krudy_froccs/",
+            },
+        ]}
+    />
+    <KoloraMemberDisplay name="Tolnai Zsófi" />
+    <KoloraMemberDisplay
+        name="Csáktornyai Ádám - Csáki"
+        tags={["Infós", "Táncos"]}
+        bio="Mindenki csak Csákiként ismer. A srác, aki furcsa módokon kombinálja az informatikát a művészettel. Slam-elek, programozok, táncolok, egyszóval alkotok. Ezt a weboldalt és a költészet napján a kincsvadászat eseményeket raktam össze. Ha érdekelnek az ilyesmi dolgok, zaklass nyugodtan! :D"
+        links={[
+            {
+                name: "WholesameWare, a saját márkám",
+                url: "https://github.com/WholesomeWare",
+            },
+        ]}
+    />
 </main>
 <Footer />
