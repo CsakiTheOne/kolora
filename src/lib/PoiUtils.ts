d1db505fc537f39f15a5d5524d707ded62c94ca3
Csáki <jockahun@gmail.com>
2025-05-01 20:56:55 +0200

    Refactor PoiUtils distances to align with user experience requirements

--- src/lib/PoiUtils.ts
+++ src/lib/PoiUtils.ts
@@ -3,12 +3,12 @@ export default class PoiUtils {
      * The maximum distance in meters from the sticker to view the poi page.
      * If the user goes out of this distance, the poi page will be closed.
      */
-    static DISTANCE_TO_VIEW = 200;
+    static DISTANCE_TO_VIEW = 150;
     /**
      * The maximum distance in meters from the sticker to open the poi page from the feeds page.
      * If the user goes out of this distance, the poi page can still be viewed, but not opened with a button.
      */
-    static DISTANCE_TO_OPEN = 50;
+    static DISTANCE_TO_OPEN = 30;
     static POST_CONTENT_LENGTH_LIMIT = 2000;
 
     /**
