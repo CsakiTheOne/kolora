7a992b387c68a4d356dba3d68795cc854d83d060
Csáki <jockahun@gmail.com>
2025-05-01 11:27:24 +0200

    Enhance LeafletMap component with improved POI popups, user location marker, and refined styling

--- src/components/poi/LeafletMap.svelte
+++ src/components/poi/LeafletMap.svelte
@@ -22,104 +22,106 @@
 {#if !isLoaded}
     <div class="loading">Loading...</div>
 {:else}
-    <div {...rest}>
-        <sveaflet.Map
-            options={{
-                center: [46.9, 18.3],
-                zoom: 7,
-            }}
-            bind:instance={map}
-        >
-            <sveaflet.TileLayer
-                url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
-            />
-            {#each pois as poi: POI}
-                <sveaflet.Marker latLng={[poi.latitude, poi.longitude]}>
+    <div>
+        <div {...rest} class="leaflet-map-container">
+            <sveaflet.Map
+                options={{
+                    center: [46.9, 18.3],
+                    zoom: 7,
+                }}
+                bind:instance={map}
+            >
+                <sveaflet.TileLayer
+                    url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+                />
+                {#each pois as poi: POI}
+                    <sveaflet.Marker latLng={[poi.latitude, poi.longitude]}>
+                        <sveaflet.Icon
+                            options={{
+                                iconUrl: mapIconPoi,
+                                iconSize: [24, 24],
+                            }}
+                        />
+                        <sveaflet.Popup>
+                            <h4>{poi.name}</h4>
+                            <br />
+                            <ul class="outlined-list">
+                                <a href={poi.googleMapsLink} target="_blank">
+                                    <li>
+                                        <span class="mdi mdi-map"></span>
+                                        Megnyitás Google Térképen
+                                    </li>
+                                </a>
+                                {#if poi.hint1Url}
+                                    <a href={poi.hint1Url} target="_blank">
+                                        <li>
+                                            <span class="mdi mdi-help"></span>
+                                            Segítség #1
+                                        </li>
+                                    </a>
+                                {/if}
+                                {#if poi.hint2Url}
+                                    <a href={poi.hint2Url} target="_blank">
+                                        <li>
+                                            <span class="mdi mdi-help"></span>
+                                            Segítség #2
+                                        </li>
+                                    </a>
+                                {/if}
+                            </ul>
+                        </sveaflet.Popup>
+                    </sveaflet.Marker>
+                    <sveaflet.Circle
+                        latLng={[poi.latitude, poi.longitude]}
+                        options={{
+                            color: "#931621",
+                            fillColor: "#931621",
+                            fillOpacity: 0.4,
+                            radius: PoiUtils.DISTANCE_TO_OPEN,
+                        }}
+                    />
+                    <sveaflet.Circle
+                        latLng={[poi.latitude, poi.longitude]}
+                        options={{
+                            color: "transparent",
+                            fillColor: "#931621",
+                            fillOpacity: 0.1,
+                            radius: PoiUtils.DISTANCE_TO_VIEW,
+                        }}
+                    />
+                {/each}
+                <sveaflet.Marker latLng={userLocation}>
                     <sveaflet.Icon
                         options={{
-                            iconUrl: mapIconPoi,
-                            iconSize: [24, 24],
+                            iconUrl: mapIconUser,
+                            iconSize: [16, 16],
                         }}
                     />
                     <sveaflet.Popup>
-                        <h4>{poi.name}</h4>
+                        <h4>Itt vagy most</h4>
                         <br />
                         <ul class="outlined-list">
-                            <a href={poi.googleMapsLink} target="_blank">
+                            <button
+                                onclick={() => {
+                                    map.setView(userLocation, 15);
+                                }}
+                            >
+                                <li>
+                                    <span class="mdi mdi-crosshairs-gps"></span>
+                                    Középre állítás
+                                </li>
+                            </button>
+                            <a href="https://maps.google.com" target="_blank">
                                 <li>
                                     <span class="mdi mdi-map"></span>
-                                    Megnyitás Google Térképen
+                                    Google Térkép megnyitása
                                 </li>
                             </a>
-                            {#if poi.hint1Url}
-                                <a href={poi.hint1Url} target="_blank">
-                                    <li>
-                                        <span class="mdi mdi-help"></span>
-                                        Segítség #1
-                                    </li>
-                                </a>
-                            {/if}
-                            {#if poi.hint2Url}
-                                <a href={poi.hint2Url} target="_blank">
-                                    <li>
-                                        <span class="mdi mdi-help"></span>
-                                        Segítség #2
-                                    </li>
-                                </a>
-                            {/if}
                         </ul>
                     </sveaflet.Popup>
                 </sveaflet.Marker>
-                <sveaflet.Circle
-                    latLng={[poi.latitude, poi.longitude]}
-                    options={{
-                        color: "#931621",
-                        fillColor: "#931621",
-                        fillOpacity: 0.4,
-                        radius: PoiUtils.DISTANCE_TO_OPEN,
-                    }}
-                />
-                <sveaflet.Circle
-                    latLng={[poi.latitude, poi.longitude]}
-                    options={{
-                        color: "transparent",
-                        fillColor: "#931621",
-                        fillOpacity: 0.1,
-                        radius: PoiUtils.DISTANCE_TO_VIEW,
-                    }}
-                />
-            {/each}
-            <sveaflet.Marker latLng={userLocation}>
-                <sveaflet.Icon
-                    options={{
-                        iconUrl: mapIconUser,
-                        iconSize: [16, 16],
-                    }}
-                />
-                <sveaflet.Popup>
-                    <h4>Itt vagy most</h4>
-                    <br />
-                    <ul class="outlined-list">
-                        <button
-                            onclick={() => {
-                                map.setView(userLocation, 15);
-                            }}
-                        >
-                            <li>
-                                <span class="mdi mdi-crosshairs-gps"></span>
-                                Középre állítás
-                            </li>
-                        </button>
-                        <a href="https://maps.google.com" target="_blank">
-                            <li>
-                                <span class="mdi mdi-map"></span>
-                                Google Térkép megnyitása
-                            </li>
-                        </a>
-                    </ul>
-                </sveaflet.Popup>
-            </sveaflet.Marker>
-        </sveaflet.Map>
+            </sveaflet.Map>
+        </div>
         <div class="button-row">
             <button
                 class="btn"
@@ -135,14 +137,32 @@
 {/if}
 
 <style>
+    .leaflet-map-container {
+        border-radius: var(--corner-radius);
+        overflow: hidden;
+    }
+
     :global(.leaflet-popup-content-wrapper, .leaflet-popup-tip) {
-        background-color: var(--background-color) !important;
-        color: var(--on-background-color) !important;
+        background-color: var(--secondary-color) !important;
+        color: var(--on-secondary-color) !important;
         border-radius: var(--corner-radius) !important;
     }
 
+    :global(.leaflet-control-zoom) {
+        background-color: var(--secondary-color) !important;
+        color: var(--on-secondary-color) !important;
+        border: none !important;
+        border-radius: var(--corner-radius) !important;
+        overflow: hidden !important;
+    }
+
+    :global(.leaflet-bar a) {
+        background-color: inherit !important;
+        color: inherit !important;
+    }
+
     :global(.leaflet-popup-close-button) {
-        color: var(--on-primary-variant-color) !important;
+        color: var(--on-secondary-color) !important;
         scale: 1.5;
     }
 
@@ -151,6 +171,8 @@
         flex-direction: row;
         justify-content: right;
         gap: var(--spacing);
+        padding-left: var(--spacing);
+        padding-right: var(--spacing);
     }
 
     .button-row button {
