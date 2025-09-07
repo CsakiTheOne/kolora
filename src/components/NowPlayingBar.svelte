c3eea7370492379b613eac6d35c46c33cf66a2d2
Csáki <jockahun@gmail.com>
2025-09-07 17:17:00 +0200

    Updated now playing

--- src/components/NowPlayingBar.svelte
+++ src/components/NowPlayingBar.svelte
@@ -6,6 +6,7 @@
     const decoAnimFramerate = 1000 / 60; // 60 FPS
 
     let currentEvent: any = $state(null);
+    let isSoon: boolean = $state(false);
 
     let decoCanvas: HTMLCanvasElement | undefined = $state();
     let ctx: CanvasRenderingContext2D | undefined = $state();
@@ -18,6 +19,13 @@
             start: new Date("2025-06-01T19:30:00"),
             end: new Date("2025-06-01T21:00:00"),
             nowPlayingText: "Maya és Chad Gustavson @ Museum Café",
+        },
+        {
+            name: "KAM",
+            location: "Museum Café",
+            start: new Date("2025-09-07T18:30:00"),
+            end: new Date("2025-09-07T19:30:00"),
+            nowPlayingText: "KAM @ Museum Café",
         },
     ];
 
@@ -41,7 +49,7 @@
                 const height =
                     (Math.sin((animTimer + i * 4) / 800) *
                         decoCanvas.clientHeight) /
-                    2;
+                    (isSoon ? 4 : 2);
                 const y = decoCanvas.clientHeight / 2 - height;
                 ctx.fillRect(i, y, 16, height);
             }
@@ -53,6 +61,18 @@
                 const now = new Date();
                 return event.start <= now && event.end >= now;
             });
+            isSoon = false;
+            if (!currentEvent) {
+                // If no current event, check next event today
+                currentEvent = tempHardcodedEvents.find((event) => {
+                    const now = new Date();
+                    return (
+                        event.start.toDateString() === now.toDateString() &&
+                        event.start > now && event.start <= new Date(now.getTime() + 60 * 60 * 1000 * 2)
+                    );
+                });
+                isSoon = true;
+            }
         });
 
         return () => {
@@ -65,7 +85,15 @@
     <div class="card" {...rest}>
         <canvas class="deco" bind:this={decoCanvas}></canvas>
         <div class="content">
-            <p><b>Now playing™️</b></p>
+            <p>
+                <b>
+                    {#if isSoon}
+                        Hamarosan kezdődik
+                    {:else}
+                        Now playing™️
+                    {/if}
+                </b>
+            </p>
             <p>{currentEvent?.nowPlayingText}</p>
         </div>
     </div>
