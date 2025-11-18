d96490ec09ba5de2c1ed0df5095eeb5f7024f733
Csáki <jockahun@gmail.com>
2025-11-18 23:29:22 +0100

    Anyám kínja styled

--- src/routes/projects/anyam-kinja/+page.svelte
+++ src/routes/projects/anyam-kinja/+page.svelte
@@ -7,7 +7,9 @@
     <main style="min-height: 100svh;">
         <h1>Anyám Kínja</h1>
         <p class="text-small">
-            Klein Anna kisfilmje a <a href="/" style="color: inherit;">Kolora Egyesület</a> támogatásával.
+            Klein Anna kisfilmje a <a href="/" style="color: inherit;"
+                >Kolora Egyesület</a
+            > támogatásával.
         </p>
         <p>
             Mit jelent gondoskodni valakiről, aki már nem a régi?<br />
@@ -33,25 +35,55 @@
             A Te segítségeddel viszont egy olyan filmet készíthetünk, amely
             méltóan beszél azokról, akik csendben hordozzák mások életét.
         </p>
+        <div class="movie-placeholder">
+            <p>Hamarosan...</p>
+        </div>
     </main>
     <Footer />
 </div>
 
 <style>
     .theme-override {
-        --primary-color: #8d552e !important;
-        --primary-variant-color: #d5c3b6 !important;
-        --secondary-color: #33214b !important;
-        --secondary-variant-color: #6ea6c4 !important;
-        --background-color: #d5c3b6 !important;
-        --on-primary-color: #d5c3b6 !important;
-        --on-primary-variant-color: #33214b !important;
-        --on-secondary-color: #d5c3b6 !important;
-        --on-secondary-variant-color: #33214b !important;
-        --on-background-color: #33214b !important;
-        --spacing: 20px !important;
-        --corner-radius: 16px !important;
+        --primary-color: #000000 !important;
+        --primary-variant-color: #333333 !important;
+        --secondary-color: #666666 !important;
+        --secondary-variant-color: #999999 !important;
+        --background-color: #ffffff !important;
+        --on-primary-color: #ffffff !important;
+        --on-primary-variant-color: #ffffff !important;
+        --on-secondary-color: #ffffff !important;
+        --on-secondary-variant-color: #000000 !important;
+        --on-background-color: #000000 !important;
+        --spacing: 24px !important;
+        --corner-radius: 4px !important;
         background-color: var(--background-color);
         color: var(--on-background-color);
+    }
+
+    h1 {
+        font-weight: 700;
+        color: #000000;
+        letter-spacing: -0.5px;
+        text-transform: uppercase;
+    }
+
+    main > p {
+        background-color: #000000;
+        color: #ffffff;
+        padding: calc(var(--spacing) * .5);
+        box-shadow: 4px 4px 0px #cccccc;
+    }
+
+    .movie-placeholder {
+        width: 100%;
+        aspect-ratio: 16 / 9;
+        border: 3px solid #000000;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-style: italic;
+        color: #000000;
+        background: #f5f5f5;
+        font-weight: 500;
     }
 </style>
