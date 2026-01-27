4f9d62630d022b2cb593589c1319ac3159c55f81
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-01-27 12:08:45 +0100

    Projects page update

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,29 +5,43 @@
 
 <Header selectedTab="Projektek" />
 <main>
-    <ul class="outlined-list">
+    <h1>Projektjeink, nagyobb eseményeink</h1>
+    <h2>Kolora Feszt 2025</h2>
+    <p>
+        Az első összművészeti fesztiválunk, ahol környékbeli alkotók és előadók
+        mutatkoztak be Székesfehérváron. Bandák, kiállítás, DJ-k és workshop-ok.
+    </p>
+    <p>
         <a href="/projects/feszt-2025">
-            <p>
-                <span class="mdi mdi-party-popper"></span>
-                2025 Kolora Feszt
-            </p>
-            <p class="text-small">
-                Az első összművészeti fesztiválunk, ahol környékbeli alkotók és
-                előadók mutatkoztak be Székesfehérváron. Bandák, kiállítás, DJ-k
-                és workshop-ok.
-            </p>
+            <button class="btn">Visszatekintés az esemény oldalára</button>
         </a>
+    </p>
+    <h2>Költészet napi kincsvadászat</h2>
+    <p>
+        A Magyar Költészet Napja alkalmából matricákat rejtettünk el
+        Székesfehérváron, amelyekhez telefont érintve verseket és egyéb
+        meglepetéseket lehetett találni.
+    </p>
+    <p>
         <a href="/stickers">
-            <p>
-                <span class="mdi mdi-nfc-variant"></span>
-                2024 Költészet napi kincsvadászat
-            </p>
-            <p class="text-small">
-                A Magyar Költészet Napja alkalmából matricákat rejtettünk el
-                Székesfehérváron, amelyekhez telefont érintve verseket és egyéb
-                meglepetéseket lehetett találni.
-            </p>
+            <button class="btn">Visszatekintés az esemény oldalára</button>
         </a>
-    </ul>
+    </p>
+    <h2>Élő hátterek és egyéb digitális alkotások</h2>
+    <p>
+        A <a href="https://instagram.com/wholesomewarestuff/" target="_blank"
+            >WholesomeWare</a
+        > lehetőséget ad a rajzolni, festeni vagy bármilyen vizuális formában alkotni
+        kedvelőknek, hogy műveiket feldobhassák egy kis interaktivitással és a brand
+        nevét viselő alkalmazásban meg is oszthatják ezeket.
+    </p>
+    <p>
+        <a
+            href="https://play.google.com/store/apps/details?id=com.csakitheone.wholesomeware&pcampaignid=web_share"
+            target="_blank"
+        >
+            <button class="btn">WholesomeWare app beszerzése</button>
+        </a>
+    </p>
 </main>
 <Footer />
