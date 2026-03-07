2148896221e2634323447604270fd25afec8e4a9
Csáki <jockahun@gmail.com>
2026-03-07 10:36:28 +0100

    Added sticker page

---
+++ src/routes/projects/kolteszet-napja-2026/sticker/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+
+    let id: string = $state('');
+
+    onMount(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        id = urlParams.get('id') || '';
+    });
+</script>
+
+<main class="container-column">
+    <h1>Költészet napi kincsvadászat 2026 - Matrica</h1>
+    <p>Matrica ID: {id}</p>
+</main>
