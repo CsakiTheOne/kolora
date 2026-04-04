be2682c17769fb5586251755e2745405223e2477
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-04-04 19:06:57 +0200

    Changes in muzi sticker

--- src/routes/projects/2026-04-11/sticker/muzi/+page.svelte
+++ src/routes/projects/2026-04-11/sticker/muzi/+page.svelte
@@ -4,6 +4,64 @@
 </script>
 
 <StickerPage>
+    <ComicPanel innerClass="container-column panel-purple">
+        <h2>LAST NIGHT I DREAMT I WAS A DJ</h2>
+        <pre>I call Miguel on the phone and ask him
+if he thinks I would be better off as a DJ or as a poet
+and Miguel answers as a Poet.
+My girlfriend also says Poet.
+My girlfriend’s brother says Poet
+and the chick I met when she was in line behind me at the movies
+says DJ.
+ 
+Girls tend to see me more as a DJ
+while the women shopping at supermarkets
+say that I should stick with the poems.
+ 
+My mother says Poet.
+The plumber says Poet.
+The five poets I know all say
+I’d be better off as a DJ.
+My sister abstained from voting.
+ 
+I went to see DJ Tiësto
+and a gringa grabbed my hands
+and said that DJs are creatures of God.
+They’re angels, she said and while she was talking
+I imagined all the DJs with their turntables
+flying around God like mosquitoes
+and God shooing them away
+with his hand.
+ 
+But the question is whether the poets and the DJs
+can be reconciled;
+if they can be One,
+if it’s possible to write poems with one hand
+and with the other scratch records,
+if it’s possible to be half-poet, half-DJ,
+to be a poet above the waist
+and below the waist a DJ
+or vice versa
+or maybe, during a full moon, a poet
+could transform into a DJ
+or maybe I’m complicating everything
+and the fact is every DJ wants to be a poet
+and every poet wants to be a DJ.
+ 
+There’s a legend in which a DJ and a poet
+fall into a well.
+They shout and shout until
+a man appears and throws down
+a rope. The DJ climbs up first but when
+they throw the rope back to the poet he screams, Leave me down here,
+and the man and the DJ do so. They wait in silence
+for a little while, and then they leave.</pre>
+        <p>- Frank Báez</p>
+        <a
+            href="https://www.poetryinternational.com/en/poets-poems/poems/poem/103-29712_LAST-NIGHT-I-DREAMT-I-WAS-A-DJ#lang-org"
+            target="_blank">Elolvasom eredeti nyelven</a
+        >
+    </ComicPanel>
     <ComicPanel innerClass="container-column">
         <h2>A borozó</h2>
         <pre>Gondüző borocska mellett
