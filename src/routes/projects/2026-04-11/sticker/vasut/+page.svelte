2b164c8235682b35c09bda2849901a7d5c40fee9
Csáki <jockahun@gmail.com>
2026-04-06 17:23:08 +0200

    Added sticker vasut

---
+++ src/routes/projects/2026-04-11/sticker/vasut/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import StickerPage from "../../../../../components/2026-04-11/StickerPage.svelte";
+</script>
+
+<StickerPage>
+    <iframe
+        class="w-full aspect-video"
+        src="https://www.youtube.com/embed/EA-OXmappUk?si=Gd_O1Q9aAejP0YJF"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+    ></iframe>
+</StickerPage>
