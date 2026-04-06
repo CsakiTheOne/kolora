bb509eb7034ac5f6b4ebc5ffc811de9c5ed71ad6
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-04-06 22:18:30 +0200

    Added sticker ciszter

---
+++ src/routes/projects/2026-04-11/sticker/ciszter/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import StickerPage from "../../../../../components/2026-04-11/StickerPage.svelte";
+</script>
+
+<StickerPage>
+    <iframe
+        class="w-full aspect-square"
+        src="https://www.youtube.com/embed/ecRO5uOWOuI?si=9EuA2vIjSLMpyH1h"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+    ></iframe>
+</StickerPage>
