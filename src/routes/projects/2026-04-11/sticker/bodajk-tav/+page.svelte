5aee4c0b4623e8b0b40063734500f77b3640633d
Csáki <jockahun@gmail.com>
2026-04-06 15:33:59 +0200

    Added sticker toparti

--- src/routes/projects/2026-04-11/sticker/bodajk-tav/+page.svelte
+++ src/routes/projects/2026-04-11/sticker/bodajk-tav/+page.svelte
@@ -54,13 +54,4 @@ Van, amikor a csend mondja ki helyettünk:
 „igen, ez az. Itt jó lenni.”</pre>
         <p>- Éles Barbara</p>
     </ComicPanel>
-    <iframe
-        class="w-full aspect-video"
-        src="https://www.youtube.com/embed/rWwfyzX2OeM?si=1uF5QsHidt3a0Z9q"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-    ></iframe>
 </StickerPage>
---
+++ src/routes/projects/2026-04-11/sticker/toparti/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import StickerPage from "../../../../../components/2026-04-11/StickerPage.svelte";
+    import ComicPanel from "../../../../../components/ComicPanel.svelte";
+</script>
+
+<StickerPage>
+    <ComicPanel innerClass="container-column panel-black">
+        <h2>Őrizem a szemed</h2>
+        <pre>Már vénülő kezemmel
+Fogom meg a kezedet,
+Már vénülő szememmel
+Őrizem a szemedet.
+ 
+Világok pusztulásán
+Ősi vad, kit rettenet
+Űz, érkeztem meg hozzád
+S várok riadtan veled.
+ 
+Már vénülő kezemmel
+Fogom meg a kezedet,
+Már vénülő szememmel
+Őrizem a szemedet.
+ 
+Nem tudom, miért, meddig
+Maradok meg még neked,
+De a kezedet fogom
+S őrizem a szemedet.</pre>
+        <p>- Ady Endre</p>
+    </ComicPanel>
+    <iframe
+        class="w-full aspect-video"
+        src="https://www.youtube.com/embed/rWwfyzX2OeM?si=1uF5QsHidt3a0Z9q"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+    ></iframe>
+</StickerPage>
