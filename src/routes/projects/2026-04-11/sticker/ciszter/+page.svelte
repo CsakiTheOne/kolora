d5a631e0d31c2f541c9c34308ea78050071a6817
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-04-07 12:36:49 +0200

    Added some poems

--- src/routes/projects/2026-04-11/sticker/ciszter/+page.svelte
+++ src/routes/projects/2026-04-11/sticker/ciszter/+page.svelte
@@ -1,8 +1,37 @@
 <script>
     import StickerPage from "../../../../../components/2026-04-11/StickerPage.svelte";
+    import ComicPanel from "../../../../../components/ComicPanel.svelte";
 </script>
 
 <StickerPage>
+    <ComicPanel innerClass="container-column panel-blue-variant">
+        <h2>Csendes csodák</h2>
+        <pre>Ne várd, hogy a föld meghasadjon
+És tűz nyelje el Sodomát.
+A mindennap kicsiny csodái
+Nagyobb és titkosabb csodák.
+ 
+Tedd a kezedet a szívedre
+Hallgasd, figyeld, hogy mit dobog,
+Ez a finom kis kalapálás
+Nem a legcsodásabb dolog?
+ 
+Nézz a sötétkék végtelenbe,
+Nézd a kis ezüstpontokat:
+Nem csoda-e, hogy árva lelked
+Feléjük szárnyat bontogat?
+ 
+Nézd, árnyékod hogy fut előled,
+Hogy nő, hogy törpül el veled.
+Nem csoda ez? - s hogy tükröződni
+Látod a vízben az eget?
+ 
+Ne várj nagy dolgot életedbe,
+Kis hópelyhek az örömök,
+Szitáló, halk szirom-csodák.
+Rajtuk át Isten szól: jövök.</pre>
+        <p>- Reményik Sándor</p>
+    </ComicPanel>
     <iframe
         class="w-full aspect-square"
         src="https://www.youtube.com/embed/ecRO5uOWOuI?si=9EuA2vIjSLMpyH1h"
--- src/routes/projects/2026-04-11/sticker/deak/+page.svelte
+++ src/routes/projects/2026-04-11/sticker/deak/+page.svelte
@@ -1,10 +1,59 @@
 <script>
     import StickerPage from "../../../../../components/2026-04-11/StickerPage.svelte";
     import aiCupcakeMeme from "$lib/images/events/2026-04-11/ai-cupcake-meme.png";
+    import ComicPanel from "../../../../../components/ComicPanel.svelte";
 </script>
 
 <StickerPage>
-    
+    <ComicPanel innerClass="container-column">
+        <h2>Marhalevél</h2>
+        <pre>Egy tehén szerelmes lett a szép bikába,
+minden vad bikának legvadabbikába.
+Vonzalmát megírta egy marhalevélben
+nagyjából eképpen:
+
+Hatalmas Barom!
+Bocsássa meg, hogy pár sorommal zavarom.
+Tudom, mily elfoglalt, milyen megbecsült Ön,
+mégis tollat ragadott csülköm,
+hogy amit a marhanyelv elbôgni nem restell,
+így adjam tudtára, Mester!
+Ön, ismervén jól a tehénszív rejtelmét,
+tudja, hogy nem minden a napi tejtermék.
+Amíg szorgalmasan duzzasztom tôgyemet,
+gondolatom egyre Ön körül ôgyeleg.
+Muú! Minden bikák közt legelôkelôbb!
+Midôn megláttam a legelô elôtt,
+elpirultam, elsápadtam,
+vágy reszketett felsálamban,
+s úgy éreztem, hogy kéj oson
+keresztül a rostélyoson.
+Muú, hogy forrt a vér szívembe,
+hogy tódult a bélszínembe!
+Az a perc, mit velem Ön tölthet maholnap,
+megrázza majd egész pörköltnekvalómat.
+Ám míg Önrôl ábrándozom kérôdzve,
+vad féltés öl a szívemig férkôzve,
+és átjárja ó mind a kín,
+velôscsontom, mócsingjaim.
+Már bánom e merész vágyat,
+hisz Ön büszke tenyészállat,
+csupa gôg,
+mely után az egész tehéncsorda bôg.
+De ne féljen Bikaságod!
+Ha nem szeret, félreállok.
+Nem fog látni levert búsnak,
+mert beállok leveshúsnak.
+Ám ha mégis kegyes szívvel veszi ezt a levelet,
+s megszánja az Önért égô tehenet,
+válaszoljon hamar rája.
+Üdvözli Önt a marhája.
+
+Im a levél. Ráírva a kelte.
+Az úton a posta elôtt le is pecsételte.
+De a postáskisasszony nem vette fel...</pre>
+        <p>- Romhányi József</p>
+    </ComicPanel>
     <iframe
         width="100%"
         style="aspect-ratio: 16 / 9;"
