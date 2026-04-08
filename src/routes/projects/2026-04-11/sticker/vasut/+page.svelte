8de6d9db5ddf32fc3907fd9cd2142cf311438987
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-04-08 14:04:00 +0200

    All stickers have content

--- src/routes/projects/2026-04-11/sticker/vasut/+page.svelte
+++ src/routes/projects/2026-04-11/sticker/vasut/+page.svelte
@@ -1,8 +1,36 @@
 <script>
     import StickerPage from "../../../../../components/2026-04-11/StickerPage.svelte";
+    import ComicPanel from "../../../../../components/ComicPanel.svelte";
 </script>
 
 <StickerPage>
+    <ComicPanel innerClass="container-column panel-red">
+        <pre>A korlátlanságban ugyan ott van néha a korlát is,
+De a lehetetlenben is, mi van, ha sikerül mégis.
+A látható mellett a láthatatlan érdemesebb-e,
+Ha nézel, de nem látsz értékeket, az veszélyes lehet.
+
+Hogyha futsz, előre szaladsz, mégis semerre sem haladsz,
+Hangosan kiáltasz, ám hangtalan és néma a válasz.
+Tudom, kerestél a színtelenben színeket, nem egyszer,
+De az igazságtalanban az igazságot sem lelted.
+
+Csak szótlan szavaid és mozdulatlan mozdulataid,
+Árulták el a gondtalanságnak tettetett gondjaid.
+Az esztelen tettek vagy az ész tétlensége nagyobb bűn?
+Lelketlen emberek, embertelen lelkek vesznek körül.
+
+Hol lelsz emberséges lelket, vagy lelkes embereket,
+Kit még az érdek érdektelensége nem szennyezett be?
+Ki lesz hát az, aki társad lesz a társtalan magányban,
+Ki reményed lesz végső reménytelenség korszakában?
+
+A valótlanság valósága, végtelen időd véges,
+Így lesz az életlen, eltompult érzékekből éles.
+Fénytelen, álmos nappalok, álmatlan, fényes éjszakák,
+Életben élettelenség, s halálban halhatatlanság.</pre>
+        <p>- Dervalics Enikő</p>
+    </ComicPanel>
     <iframe
         class="w-full aspect-video"
         src="https://www.youtube.com/embed/EA-OXmappUk?si=Gd_O1Q9aAejP0YJF"
