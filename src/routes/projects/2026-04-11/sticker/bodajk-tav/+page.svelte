1c72268daca86a717eccbf04cdd6154dfa72e4ff
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-04-09 10:50:00 +0200

    Added poem to pond

--- src/routes/projects/2026-04-11/sticker/bodajk-tav/+page.svelte
+++ src/routes/projects/2026-04-11/sticker/bodajk-tav/+page.svelte
@@ -73,4 +73,77 @@ Van, amikor a csend mondja ki helyettünk:
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
     ></iframe>
+    <ComicPanel>
+        <h2>Szerelmes klisé-átdolgozás</h2>
+        <pre>Azt kívánom, hogy legyünk 85 évesek
+Úgy legalább nem olyan ijesztő az a maradék élet sem.
+Addigra majd a színes üvegablakaink előtt fogunk ülni a teraszunk árnyékában szappan illatúan 
+és esténként még mindig tovább visszük a már amúgyis 23765 napos Duolingonkat
+Vagy 30 ipszit hallgatunk egy hintaágyban összebújva
+A szóvicceidet feljegyezve őrzöm majd a könyvtárszobánk egyik eldugott sarkában és onnan emlékszem vissza
+Mert most életkorom 20 sincs 
+és már én sem tudok olyat írni amit nem írt meg az Úrfi
+
+Ezeket a sorokat is csak elloptam Akár a szívedet, 
+de mostanra már talán megszoktad hogy lopom a legtöbb rímedet
+Is, meg a szekrényből az ingedet
+És ha jobban áll rajtam max kicsit megsértődsz
+Eldobtam a cigarettát is, ne bántsa az asztmád
+De én sem tudok olyan írni amit nem írt meg a 
+Budapest bár
+
+Veled is csak egy húszezer éjszakás kaland lenne elég
+S ha csillagosra vált az ég
+Hát miénk már a nyolcas is
+Várnak minket Hajmáskér tornyai
+Téli estén közös vacogás
+Szobrok alján fiatal románc
+Szívünk ritmusa együtt disztichon
+De én sem tudok olyan írni amit nem írt meg a 30 ipszilon
+
+Hozzám a szerelem csak pihenni tért be
+Nem rohant, és az idillben megérett
+Szívemben szobát bérelt
+kavicssal díszítette S képpel
+Kézen fogva sétálgatunk Pécsett
+Lábunk egymás után egyszerre lépdel
+Balkáni érzet a Széchenyi téren
+Magasan a Tv torony, mindenki megpillantja
+De így sem tudok olyan írni amit nem írt meg Cserihanna
+
+Köszönöm hogy elviselsz amikor 
+úgy érzem tényleg nem lehet
+Hogy kitartasz és annyiszor 
+Vigasztalsz ahányszor kellhet
+A legnagyobb félelmem 
+Hogy ott hagynak egyedül
+Alábbhagy, mert te felemelsz
+És az összes fájdalmam menekül
+Terápiából írok, és ez nagy ritkán helyrerak
+De én sem tudok olyan írni amit nem írt meg Csepella
+
+Te vagy itt a legsármosabb talpadtól a hajadig
+Nem megy nekem az átírás, kitalálok valamit
+Legalább te érzed, hogy a nagy izgalomban
+A kedvedért rohadt finom illatom van
+Esténként szemeid apró lámpácskák
+De nem tudok olyan írni amit nem énekelt el a Rájátszás
+
+Bőröd melege nyugtatja a testem
+Simítod hajamat és a csendben
+Szívemhez szorítom a tested
+Üti a ritmust a szíved rendben
+Mert velem még nem bánt ilyen jól ember
+Az érzéssel először nem tudtam mit kezdjek 
+Nem tudom elmondani mennyire jó
+Nem lehet leírni és csak álmodozom mert
+Sötét van akkor amikor vége van
+Rohadt sötét mert a szem csukva van</pre>
+        <p>
+            - Horváth Eszter (<a
+                href="https://www.instagram.com/odalokottkolteszet/"
+                target="_blank">Odalökött Költészet</a
+            >)
+        </p>
+    </ComicPanel>
 </StickerPage>
