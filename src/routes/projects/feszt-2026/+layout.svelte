9e20f7f4c1c31ed7a197d442cf514bd51367d90d
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2026-08-24 16:25:13 +0200

    Hidden white lines on Safari

--- src/routes/projects/feszt-2026/+layout.svelte
+++ src/routes/projects/feszt-2026/+layout.svelte
@@ -48,6 +48,10 @@
 <style>
     @import url("https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap");
 
+    :global(body) {
+        background-color: #0b5d63;
+    }
+
     :global(main) {
         width: 100%;
         position: relative;
